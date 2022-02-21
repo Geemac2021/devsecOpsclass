71ec2c2389a5f717bae343b16d4e671ffc5f180e
Geemac2021 <geeuwaifo@gmail.com>
2022-02-20 22:02:22 -0500

    daily1

---
+++ daily20.py
@@ -0,0 +1,54 @@
+Keyword	Description
+and	A logical operator
+as	To create an alias
+assert	For debugging
+break	To break out of a loop
+class	To define a class
+continue	To continue to the next iteration of a loop
+def	To define a function
+del	To delete an object
+elif	Used in conditional statements, same as else if
+else	Used in conditional statements
+except	Used with exceptions, what to do when an exception occurs
+False	Boolean value, result of comparison operations
+finally	Used with exceptions, a block of code that will be executed no matter if there is an exception or not
+for	To create a for loop
+from	To import specific parts of a module
+global	To declare a global variable
+if	To make a conditional statement
+import	To import a module
+in	To check if a value is present in a list, tuple, etc.
+is	To test if two variables are equal
+lambda	To create an anonymous function
+None	Represents a null value
+nonlocal	To declare a non-local variable
+not	A logical operator
+or	A logical operator
+pass	A null statement, a statement that will do nothing
+raise	To raise an exception
+return	To exit a function and return a value
+True	Boolean value, result of comparison operations
+try	To make a try...except statement
+while	To create a while loop
+with	Used to simplify exception handling
+yield	To end a function, returns a generator
+
+
+zip
+The zip object yields n-length tuples, where n is the number of iterables
+  passed as positional arguments to zip().  The i-th element in every tuple
+ comes from the i-th iterable argument to zip().  This continues until the
+ shortest argument is exhausted.
+
+slice
+     Assuming a sequence of length len, calculate the start and stop
+     indices, and the stride length of the extended slice described by
+     S. Out of bounds indices are clipped in a manner consistent with the
+     handling of normal slices.
+Sum
+(iterable, /, start=0)
+    Return the sum of a 'start' value (default: 0) plus an iterable of numbers
+    
+    When the iterable is empty, return the start value.
+    This function is intended specifically for use with numeric values and may
+    reject non-numeric types.
