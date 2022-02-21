76a95864d11eadf267706d5dd8012c14b1e954a8
Geemac2021 <geeuwaifo@gmail.com>
2022-02-20 22:34:38 -0500

    builtin

---
+++ builtin.py
@@ -0,0 +1,18 @@
+zip
+The zip object yields n-length tuples, where n is the number of iterables
+  passed as positional arguments to zip().  The i-th element in every tuple
+ comes from the i-th iterable argument to zip().  This continues until the
+ shortest argument is exhausted.
+
+slice
+     Assuming a sequence of length len, calculate the start and stop
+     indices, and the stride length of the extended slice described by
+     S. Out of bounds indices are clipped in a manner consistent with the
+     handling of normal slices.
+Sum
+(iterable, /, start=0)
+    Return the sum of a 'start' value (default: 0) plus an iterable of numbers
+    
+    When the iterable is empty, return the start value.
+    This function is intended specifically for use with numeric values and may
+    reject non-numeric types.
